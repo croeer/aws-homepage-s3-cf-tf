202a6eb5ad6476b8e82122544139b8c74089946d
croeer <christian@roeer.info>
2025-01-13 21:11:41 +0100

    Add dynamic viewer certificate configuration and new variables for ACM certificate and custom domain names

--- cloudfront.tf
+++ cloudfront.tf
@@ -47,10 +47,29 @@ resource "aws_cloudfront_distribution" "this" {
     }
   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
+  dynamic "viewer_certificate" {
+    for_each = var.acm_certificate_arn != "" ? [1] : []
+
+    content {
+      acm_certificate_arn            = var.acm_certificate_arn
+      cloudfront_default_certificate = false
+      ssl_support_method             = "sni-only"
+      minimum_protocol_version       = "TLSv1.2_2021"
+    }
   }
 
-  price_class = "PriceClass_200"
+  dynamic "viewer_certificate" {
+    for_each = var.acm_certificate_arn == "" ? [1] : []
+
+    content {
+      cloudfront_default_certificate = true
+
+    }
+  }
+
+  aliases = var.custom_domain_names
+
+
+  price_class = "PriceClass_100"
 
 }
--- variables.tf
+++ variables.tf
@@ -8,3 +8,15 @@ variable "region" {
   type        = string
   default     = "eu-central1"
 }
+
+variable "acm_certificate_arn" {
+  description = "The ARN of the ACM certificate for the API Gateway custom domain (us-east-1)."
+  type        = string
+  default     = ""
+}
+
+variable "custom_domain_names" {
+  description = "The custom domain name for the API Gateway."
+  type        = list(string)
+  default     = []
+}
